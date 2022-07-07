66a4534b8856a101fb5fe436220a482bff80f460
kevin-doolaeghe <doolaeghe.kevin@gmail.com>
2022-07-07 16:04:59 +0200

    Add project files.

---
+++ OpcUaSampleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Opc.UaFx.Client;
+
+namespace OpcUa {
+
+    public class Program {
+
+        public static void Main() {
+            string url = "opc.tcp://opcua.demo-this.com:51210/UA/SampleServer";
+            string dp = "ns=2; i=10239";
+
+            try {
+                Console.WriteLine("OPC UA sample client started successfully.");
+                Console.WriteLine("Trying to connect to: " + url);
+                using (var client = new OpcClient(url)) {
+                    client.Connected += (s, e) => Console.WriteLine("Successfully connected to server!");
+                    client.Disconnected += (s, e) => Console.WriteLine("Disconnected from server..");
+                    client.Connect();
+
+                    Console.WriteLine("Listening for datapoint: " + dp);
+                    while (true) {
+                        var value = client.ReadNode(dp);
+                        Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " - " + value);
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            } finally {
+                Console.WriteLine("Exiting..");
+                Console.ReadKey();
+            }
+        }
+    }
+}
